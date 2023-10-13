461b553855d91a83432151d1928efe904efe2f18
tenways <73358533+ten-ways@users.noreply.github.com>
2023-10-13 08:08:56 +0100

    Fix message formatting
    Also added a fix for some blocking behaviour if message takes a while to format

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord, re, os
 from datetime import datetime
 from dotenv import load_dotenv
+from asyncio import get_event_loop
 
 regex = '(?:https://)(?:twitter.com|x.com)/(\w+)/status/(\d+)(?:\?(t|s|fbclid)=\w+)?(?:&s=[0-9]+)?'
 
@@ -11,44 +12,38 @@ bot = discord.Bot(
     intents=intents 
 )
 
+def parse_message(message):
+    content = message.content
+    match = re.search(regex, content)
+    if not match: return
+
+    replacement_message = content[:match.start()] + f"https://fixupx.com/{match.group(1)}/status/{match.group(2)}" + content[match.end():]
+
+    if len(content) > match.end() - match.start(): # if link is not the only thing in the message, parse message to quote paragraphs.
+        replacement_message = "".join([ f"\n> {paragraph}" if paragraph != "" else "\n" for paragraph in replacement_message.split("\n") ])
+        replacement_message = f"## {message.author.mention}:speech_balloon: \n" + replacement_message
+    else:
+        replacement_message = f"## {message.author.mention}\n" + replacement_message
+
+    return replacement_message
+
 @bot.event
 async def on_ready():
-    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
-    print("------")
+    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}-> Logged in as {bot.user} (Bot ID: {bot.user.id})")
 
 @bot.event
-async def on_message(message: discord.Message):
+async def on_message(message):
+    if message.author.id == bot.user.id: return
 
-    if message.author.id == bot.user.id:
-        return
+    # Parse messages on separate thread as the code is slow and blocks event loop
+    if not (replacement_message := await get_event_loop().run_in_executor(None, parse_message, message)): return
 
-    if re.search(regex, message.content):
-        msgQuote = False
-        msg = []
-        now = datetime.now()
-
-        for _ in re.split('(\s)', message.content):
-            if y := re.match(regex, _):
-                msg.append(re.sub(regex, f'https://fixupx.com/{y.group(1)}/status/{y.group(2)}', _))
-                print(now.strftime("%Y-%m-%d %H:%M:%S") + f": Detected: {y.group(0)}")
-            else:
-                msgQuote = True
-                msg.append(_)
-        fixed = ''.join(msg)
-
-        try:
-            if msgQuote:
-                await message.channel.send(f"## {message.author.mention}:speech_balloon: \n{fixed}")
-            else:
-                await message.channel.send(f"## {message.author.mention}\n{fixed}")
-        except discord.errors.Forbidden: 
-            print(now.strftime("%Y-%m-%d %H:%M:%S") + ': Unable to send message in channel (permissions error).')
-            return
-        try:
-            await message.delete()
-        except discord.errors.Forbidden:
-            print(now.strftime("%Y-%m-%d %H:%M:%S") + ': Unable to delete original message (permissions error).')
-            return
+    try:
+        await message.channel.send(replacement_message)
+        await message.delete()
+        print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}-> Fixed new link! (Message ID: {message.id})")
+    except discord.errors.Forbidden as e:
+        print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}-> {e}")
 
 if __name__ == '__main__':
     load_dotenv()

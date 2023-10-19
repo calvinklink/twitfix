2adae5a6bd97f53a44b102893849830ed61f4986
tenways <73358533+ten-ways@users.noreply.github.com>
2023-10-19 18:07:26 +0100

    Message replies

--- main.py
+++ main.py
@@ -12,17 +12,6 @@ bot = discord.Bot(
     intents=intents 
 )
 
-def parse_message(message):
-    content = message.content
-
-    replacement_message = re.sub(regex, lambda match: f'https://fixupx.com/{match.group(1)}/status/{match.group(2)}', content)
-    if (replacement_message == content): return
-
-    replacement_message = "".join([ f"\n> {paragraph}" if paragraph != "" else "\n" for paragraph in replacement_message.split("\n") ])
-    replacement_message = f"## {message.author.mention}:speech_balloon: \n" + replacement_message
-
-    return replacement_message
-
 @bot.event
 async def on_ready():
     print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}-> Logged in as {bot.user} (Bot ID: {bot.user.id})")
@@ -31,11 +20,22 @@ async def on_ready():
 async def on_message(message):
     if message.author.id == bot.user.id: return
 
-    # Parse messages on separate thread as the code is slow and blocks event loop
-    if not (replacement_message := await get_event_loop().run_in_executor(None, parse_message, message)): return
+    content = message.content
+        
+    replacement_message = re.sub(regex, lambda match: f'https://fixupx.com/{match.group(1)}/status/{match.group(2)}', content)
+    if (replacement_message == content): return
+
+    replacement_message = "".join([ f"\n> {paragraph}" if paragraph != "" else "\n" for paragraph in replacement_message.split("\n") ])
+    replacement_message = f"## {message.author.mention}:speech_balloon: \n" + replacement_message
 
     try:
-        await message.channel.send(replacement_message)
+
+        if message.reference:
+            reply = await message.channel.fetch_message(message.reference.message_id)
+            await reply.reply(replacement_message)
+        else:
+            await message.channel.send(replacement_message)
+            
         await message.delete()
         print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}-> Fixed new link! (Message ID: {message.id})")
     except discord.errors.Forbidden as e:
